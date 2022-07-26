7017a2dd2969bffebd2ed1b19a0c8566017c9403
adnaan-said <adnaan009@gmail.com>
2022-07-26 15:57:22 +0400

    clean up

--- src/index.tsx
+++ src/index.tsx
@@ -9,5 +9,7 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
+  <QueryClientProvider client={client}>
     <App />
+  </QueryClientProvider>
 );
